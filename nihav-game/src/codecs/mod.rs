3e52414f742a961885778d9c851224176a851405
Kostya Shishkov <kostya.shishkov@gmail.com>
2020-04-19 10:04:04 +0200

    add Lernout & Hauspie StreamTalk 25kbps at 22 kHz codec

--- nihav-game/src/codecs/mod.rs
+++ nihav-game/src/codecs/mod.rs
@@ -10,6 +10,8 @@ pub mod bmv;
 pub mod bmv3;
 #[cfg(feature="decoder_gdvvid")]
 pub mod gremlinvideo;
+#[cfg(feature="decoder_lhst500f22")]
+pub mod lhst500f22;
 #[cfg(feature="decoder_midivid")]
 pub mod midivid;
 #[cfg(feature="decoder_midivid3")]
@@ -34,6 +36,8 @@ const GAME_CODECS: &[DecoderInfo] = &[
     DecoderInfo { name: "vmd-audio", get_decoder: vmd::get_decoder_audio },
 #[cfg(feature="decoder_vmd")]
     DecoderInfo { name: "vmd-video", get_decoder: vmd::get_decoder_video },
+#[cfg(feature="decoder_lhst500f22")]
+    DecoderInfo { name: "lhst500f22", get_decoder: lhst500f22::get_decoder },
 #[cfg(feature="decoder_midivid")]
     DecoderInfo { name: "midivid", get_decoder: midivid::get_decoder_video },
 #[cfg(feature="decoder_midivid3")]
